ce9563e1e81f2bbb48837c02d488349c0a1dbbcb
hado0123 <hado_123@naver.com>
2025-07-10 14:56:37 +0900

    up

--- ch05/sns-frontend/src/components/auth/Login.jsx
+++ ch05/sns-frontend/src/components/auth/Login.jsx
@@ -1,5 +1,50 @@
+import { TextField, Button, Container, Typography, CircularProgress } from '@mui/material'
+import { useState } from 'react'
+
 function Login() {
-    return (  );
+   const [email, setEmail] = useState('') // 이메일
+   const [password, setPassword] = useState('') //비밀번호
+
+   // 로그인 버튼 눌렀을때
+   const handleLogin = (e) => {}
+
+   return (
+      <Container maxWidth="sm">
+         <Typography variant="h4" gutterBottom>
+            로그인
+         </Typography>
+
+         {/* {error && (
+            <Typography color="error" align="center">
+               {error}
+            </Typography>
+         )} */}
+
+         <form onSubmit={handleLogin}>
+            <TextField label="이메일" name="email" fullWidth margin="normal" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+            <TextField label="비밀번호" type="password" name="password" fullWidth margin="normal" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+            {/* <Button variant="contained" color="primary" type="submit" fullWidth disabled={loading} sx={{ position: 'relative', marginTop: '20px' }}>
+               {loading ? (
+                  <CircularProgress
+                     size={24}
+                     sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                     }}
+                  />
+               ) : (
+                  '로그인'
+               )}
+            </Button> */}
+         </form>
+
+         <p>계정이 없으신가요? 회원가입</p>
+      </Container>
+   )
 }
 
-export default Login;
+export default Login
