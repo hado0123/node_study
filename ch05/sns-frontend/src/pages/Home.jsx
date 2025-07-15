c0db379305ba6d81722b1b4178d0d347c7faf5ad
hado0123 <hado_123@naver.com>
2025-07-15 15:51:28 +0900

    up

--- ch05/sns-frontend/src/pages/Home.jsx
+++ ch05/sns-frontend/src/pages/Home.jsx
@@ -13,6 +13,11 @@ function Home({ isAuthenticated, user }) {
       dispatch(fetchPostsThunk(page)) // 전체 게시물 리스트 가져오기
    }, [dispatch, page])
 
+   // 페이지 변경
+   const handlePageChange = (event, value) => {
+      setPage(value)
+   }
+
    return (
       <Container maxWidth="xs">
          <Typography variant="h4" align="center" gutterBottom>
@@ -36,6 +41,13 @@ function Home({ isAuthenticated, user }) {
                {posts.map((post) => (
                   <PostItem key={post.id} post={post} isAuthenticated={isAuthenticated} user={user} />
                ))}
+               <Stack spacing={2} sx={{ mt: 3, alignItems: 'center' }}>
+                  <Pagination
+                     count={pagination.totalPages} // 총 페이지 수
+                     page={page} // 현재 페이지
+                     onChange={handlePageChange} // 페이지를 변경할 함수
+                  />
+               </Stack>
             </>
          ) : (
             // posts 데이터가 0개 이면서 로딩중이 아닐때
