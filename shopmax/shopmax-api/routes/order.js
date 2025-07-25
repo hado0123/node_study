fbdf72cf0433816787e29ff22d8434196a77af9c
hado0123 <hado_123@naver.com>
2025-07-25 13:20:05 +0900

    up

--- shopmax/shopmax-api/routes/order.js
+++ shopmax/shopmax-api/routes/order.js
@@ -125,4 +125,67 @@ router.post('/', isLoggedIn, async (req, res, next) => {
    }
 })
 
+// localhost:8000/order/list?page=1&limit=5&startDate=2025-01-01&endDate=2025-01-16
+// 주문 목록(페이징, 날짜 검색)
+router.get('/list', isLoggedIn, async (req, res, next) => {
+   try {
+      const page = parseInt(req.query.page, 10) || 1
+      const limit = parseInt(req.query.limit, 10) || 5
+      const offset = (page - 1) * limit
+      const startDate = req.query.startDate // YYYY-MM-DD 00:00:00
+      const endDate = req.query.endDate
+      const endDateTime = `${endDate} 23:59:59` // 년월일만 받을시 시분초는 00:00:00으로 인식하므로 시간 변경(endDate 날짜에 주문한 내용도 검색되도록)
+
+      const count = await Order.count({
+         where: {
+            userId: req.user.id, // 주문자 id(pk)
+            ...(startDate && endDate ? { orderDate: { [Op.between]: [startDate, endDateTime] } } : {}), // 날짜 검색
+         },
+      })
+
+      const orders = await Order.findAll({
+         where: {
+            userId: req.user.id, // 주문자 id(pk)
+            ...(startDate && endDate ? { orderDate: { [Op.between]: [startDate, endDateTime] } } : {}), // 날짜 검색
+         },
+         limit,
+         offset,
+         include: [
+            {
+               model: Item,
+               attributes: ['id', 'itemNm', 'price'],
+               // 교차테이블 데이터(OrderItem 테이블 에서 필요한 컬럼 선택)
+               through: {
+                  attributes: ['count', 'orderPrice'],
+               },
+               include: [
+                  {
+                     model: Img,
+                     attributes: ['imgUrl'],
+                     where: { repImgYn: 'Y' }, // 대표이미지만 가져온다
+                  },
+               ],
+            },
+         ],
+         order: [['orderDate', 'DESC']], // 최근 주문내역이 먼저 오도록
+      })
+
+      res.json({
+         success: true,
+         message: '주문 목록 조회 성공',
+         orders,
+         pagination: {
+            totalOrders: count,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page,
+            limit,
+         },
+      })
+   } catch (error) {
+      error.status = 500
+      error.message = '주문 내역을 불러오는 중 오류가 발생했습니다.'
+      next(error)
+   }
+})
+
 module.exports = router
--- shopmax/shopmax-frontend/src/components/my/OrderList.jsx
+++ shopmax/shopmax-frontend/src/components/my/OrderList.jsx
@@ -5,10 +5,31 @@ import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import dayjs from 'dayjs' //날짜 시간 포맷해주는 패키지
 import 'dayjs/locale/ko' // 한글 로케일 불러오기
+import { useState } from 'react'
 
 function OrderList() {
+   const [startDate, setStartDate] = useState(null) // 시작날짜(포맷전)
+   const [endDate, setEndDate] = useState(null) // 끝 날짜(포맷전)
+   const [formattedStartDate, setFormattedStartDate] = useState('') // 시작날짜(포맷후)
+   const [formattedEndDate, setFormattedEndDate] = useState('') // 끝날짜(포맷후)
+
+   //날짜 데이터를 포맷
+   const handleDateFilter = () => {
+      // 시작일 또는 종료일 값이 null일 경우
+      if (!startDate || !endDate) {
+         alert('시작일과 종료일을 지정하세요!')
+         return
+      }
+
+      console.log(`startDate: ${startDate}, endDate: ${endDate}`)
+
+      // YYYY-MM-DD 포맷으로 변환
+      setFormattedStartDate(dayjs(startDate).format('YYYY-MM-DD'))
+      setFormattedEndDate(dayjs(endDate).format('YYYY-MM-DD'))
+   }
+
    return (
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
          <Box p={2}>
             {/* 날짜 검색 */}
             <Box
@@ -22,9 +43,11 @@ function OrderList() {
                   mt: 5,
                }}
             >
-               <DatePicker label="시작일" />
-               <DatePicker label="종료일" />
-               <Button variant="contained">날짜 검색</Button>
+               <DatePicker label="시작일" value={startDate} onChange={(newValue) => setStartDate(newValue)} sx={{ width: '35%' }} />
+               <DatePicker label="종료일" value={endDate} onChange={(newValue) => setEndDate(newValue)} sx={{ width: '35%' }} />
+               <Button variant="contained" onClick={handleDateFilter}>
+                  날짜 검색
+               </Button>
             </Box>
          </Box>
       </LocalizationProvider>
